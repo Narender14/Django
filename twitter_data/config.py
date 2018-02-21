4d8bbc3c0c33c3dae016cf54229dacc471bd0da7
Narender14 <narender1412@gmail.com>
2018-02-21 16:52:40 +0530

    Changes

--- twitter_data/config.py
+++ twitter_data/config.py
@@ -7,8 +7,8 @@ MONGO_PASS = os.getenv('MONGO_PASS', '')
 MONGO_DATABASE = os.getenv('MONGO_DATABASE', 'twitter')
 MONGO_COLLECTION = os.getenv('MONGO_COLLECTION', 'tweet')
 
-# Twitter API credentials
-# consumer_key = "yYaDlzvkMzxsPQq3aKvPLu8jm"
-# consumer_secret = "UVGdDqVxYkprPKvluMq3ArUrLlwPXd8wZ1AnNIoUl5HX5IVUea"
-# access_key = "1178472560-vVakpXvCgjzij4IXnjtBGRjUhHm3tvQzlZXYhNa"
-# access_secret = "f7tMZ63aouT6paK2yAk3310a5ukw000sUv1VOH96uQyaH"
+#Twitter API credentials
+consumer_key = "c_key"
+consumer_secret = "c_s"
+access_key = "a_key"
+access_secret = "a_s"
